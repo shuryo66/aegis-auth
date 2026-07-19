93a21368a3186f9bc94f2390b079956e4beff0bf
shuryo66 <graphovichwork@gmail.com>
2026-07-19 16:58:57 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const allowedHwids = [
+    "abef8842331bfcdb0b676b40cfd2ba83bafd04d147d1c9c14fbe3b3370549a75"
+];
+
+app.get('/auth.php', (req, res) => {
+    const userHwid = req.query.hwid;
+
+    if (userHwid && allowedHwids.includes(userHwid)) {
+        res.send("ACCESS_GRANTED_SECURE_KEY_1337");
+    } else {
+        res.send("ACCESS_DENIED");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
