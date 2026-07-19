93c158ab411125a5069db6162812f0c5f175d74e
shuryo66 <graphovichwork@gmail.com>
2026-07-19 17:01:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedHwids = [
-    "abef8842331bfcdb0b676b40cfd2ba83bafd04d147d1c9c14fbe3b3370549a75"
-];
-
 app.get('/auth.php', (req, res) => {
     const userHwid = req.query.hwid;
+
+    const rawHwids = process.env.ALLOWED_HWIDS || "";
+    
+    const allowedHwids = rawHwids.split(',').map(id => id.trim());
 
     if (userHwid && allowedHwids.includes(userHwid)) {
         res.send("ACCESS_GRANTED_SECURE_KEY_1337");
