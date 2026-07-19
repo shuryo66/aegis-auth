5017c0a47a813d2184582604899715480c45f22b
shuryo66 <graphovichwork@gmail.com>
2026-07-19 17:33:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,19 @@ app.get('/auth.php', (req, res) => {
     const userHwid = req.query.hwid ? req.query.hwid.trim().toLowerCase() : "";
     const rawHwids = process.env.ALLOWED_HWIDS || "";
     
-    // Переменная для хранения найденной роли
     let userRole = null;
 
-    // Парсим строку вида: hwid:role,hwid:role
     const entries = rawHwids.split(',').map(item => item.trim());
     
     for (const entry of entries) {
         const [hwid, role] = entry.split(':');
         if (hwid && hwid.trim().toLowerCase() === userHwid) {
-            userRole = role ? role.trim().toLowerCase() : 'user'; // по умолчанию 'user'
+            userRole = role ? role.trim().toLowerCase() : 'user'; 
             break;
         }
     }
 
     if (userRole) {
-        // Если HWID найден, отдаем JSON с успешным статусом и ролью
         res.json({
             status: "success",
             key: "ACCESS_GRANTED_SECURE_KEY_1337",
